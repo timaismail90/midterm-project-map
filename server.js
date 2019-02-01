cf641a7600b382b09fd0a7111291dccda608a2cf
fatuma ismail <timaismail90@gmail.com>
2019-02-01 22:01:19 +0000

    database tables migrated correctly; added login and logout requests and its template

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const sass = require("node-sass-middleware");
+const app = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
@@ -25,7 +25,7 @@ let mapId = "";
 
 let templateVars;
 
-let mapName="";
+let mapName = "";
 
 
 // import * as L from 'leaflet';
@@ -60,14 +60,14 @@ app.use("/api/users", usersRoutes(knex));
 
 // Explor page, is user is logged in redirect to new page. else render guest explore page.
 app.get("/", (request, respond) => {
-  if(request.session.user_id){
+  if (request.session.user_id) {
     respond.redirect('/:id');
   } else {
     knex('maps').then(maps => {
-    console.log(maps[0]);
+      console.log(maps[0]);
     });
     knex('pins').then(pins => {
-    console.log(pins);
+      console.log(pins);
     });
     respond.render('index')
   };
@@ -89,7 +89,7 @@ app.get("/", (request, respond) => {
 
 //view specic map page
 app.get('/map/:id', (request, respond) => {
-  if(request.session.user_id){
+  if (request.session.user_id) {
     //load specific map from database
     // let templateVars = {user_id: request.session.user_id,
     //                     mapId: maps[request.params.id]
@@ -97,30 +97,106 @@ app.get('/map/:id', (request, respond) => {
     respond.render('mapsEdit', templateVars); //on new create map, if null show "enter info etc."
   } else {
     knex('pins')
-    .where('maps_id', request.params.id)
-    .then(pins => {
-    knex('maps')
-    .where('id', request.params.id)
-    .then(maps => {
-      mapName = maps[0].name;
-    })
+      .where('maps_id', request.params.id)
+      .then(pins => {
+        knex('maps')
+          .where('id', request.params.id)
+          .then(maps => {
+            mapName = maps[0].name;
+          })
 
-    templateVars = {
-      pins: pins,
-      length:pins.length,
-      name:mapName
-    }
+        templateVars = {
+          pins: pins,
+          length: pins.length,
+          name: mapName
+        }
 
-    respond.render('mapShow', templateVars);
-    });
+        respond.render('mapShow', templateVars);
+      });
 
   };
 });
 
 
-    // knex('pins').where('maps_id', request.params.id).then(pins => {
-    //   console.log("hello", pins[0]);
-    // });
+
+app.get('/login', (req, res) => {
+  if (!req.session.user_id) {
+    res.render('index', );
+  } else {
+    res.redirect('mapShow')
+  }
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.name
+  knex('users').where('name', req.body.username)
+    .then(users => {
+      // user_id = users.id
+
+      req.session.id = users.id
+
+      res.redirect("/");
+    })
+});
+// knex('users').where('name', req.body.user)
+//   .where('title', 'Hello')
+//   .where({ title: 'Hello' })
+//   .whereIn('id', [1, 2, 3])
+//   .whereNot(···)
+
+
+
+
+
+
+
+
+
+
+// app.post('/login', (req, res) => {
+//   let username = ''
+//   knex('users').where('name', req.body.username)
+//     .then((results) => {
+//       knex('users').where('id', req.params.id)
+//       user_id = req.session.id
+//       let templateVars = {
+//         username: username
+//       }
+
+//       // if (req.session.id) {
+//       //   req.session.id = user_id
+//       //   res.redirect('/map/1', templateVars)
+
+//     })
+//   res.redirect('map/1', templateVars)
+// });
+
+
+
+//   })
+//   // let username = req.body.username
+//   console.log(username);
+//   let templateVars = { username: username }
+//   // let password = req.body.password;
+//   if (req.session.id) {
+//     req.session.id = user_id
+//     res.redirect('/map/1', templateVars)
+//   }
+// });
+
+
+
+
+app.post('/logout', (req, res) => {
+  if (!req.session.id)
+    res.status(302).redirect('/');
+});
+
+
+
+// knex('pins').where('maps_id', request.params.id).then(pins => {
+//   console.log("hello", pins[0]);
+// });
 
 
 // //for creating a map
@@ -148,4 +224,3 @@ app.get('/map/:id', (request, respond) => {
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
-
